83e5eba54a78ef660a18f35a7120d3dbf1b022bc
Zulhardiyanto Hardi <zulhardiyanto.achmad@bts.id>
2020-03-29 06:38:10 +0700

    Updates

---
+++ precache-manifest.9b70792e3f6f9019bba5b82246d3fc2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdec2395338bf85a600beea2ecffde64",
+    "url": "/hardizulhardiyanto/unipi-app/index.html"
+  },
+  {
+    "revision": "b781d9bf05944b94d9c7",
+    "url": "/hardizulhardiyanto/unipi-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d217841bc5f1541fd605",
+    "url": "/hardizulhardiyanto/unipi-app/static/js/2.3d7c0385.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hardizulhardiyanto/unipi-app/static/js/2.3d7c0385.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b781d9bf05944b94d9c7",
+    "url": "/hardizulhardiyanto/unipi-app/static/js/main.4117152b.chunk.js"
+  },
+  {
+    "revision": "0a416dd2905a778f3d02",
+    "url": "/hardizulhardiyanto/unipi-app/static/js/runtime-main.0a47923a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hardizulhardiyanto/unipi-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
